6dc7fe4e895ec691e1388a46b41d9c77987294e4
Camila Ruiz <ruizvillelac@g.cofc.edu>
2025-03-13 18:52:56 -0400

    flask code

--- app.py
+++ app.py
@@ -2,13 +2,19 @@
 from flask import Flask
 import os
 
+
 print("helloWorld!")
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+DETECTED_FRAMES_FOLDER = "detected_frames"
+
 
 # Ensure upload folder exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(DETECTED_FRAMES_FOLDER, exist_ok=True)
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
 
 #@app.route("/upload", methods=["POST"])
